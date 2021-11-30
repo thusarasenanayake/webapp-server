b8faa0b3cd8ed91af7661d09c41943cb686cc81b
Buddhika <buddhikad1999@gmail.com>
2021-11-30 17:56:57 +0530

    category server side  single image uploading done

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -3,9 +3,16 @@ const httpStatus = require('http-status')
 const { query } = require('express')
 
 exports.create = async (req, res, next) => {
+  const fileName = req.file.filename
+  const protocol = req.protocol
+  const host = req.get('host')
+  //const basePath= '${req.protocol}://${req.get('host')}/public/uploads/'
+  const basePath = protocol + '://' + host + '/public/uploads/'
   try {
-    const categoryData = req.body
-    const category = new Category(categoryData)
+    const category = new Category({
+      categoryName: req.body.categoryName,
+      image: basePath + fileName,
+    })
     await category.save()
     return res.status(httpStatus.CREATED).json({ category })
   } catch (error) {
@@ -37,19 +44,32 @@ exports.list = async (req, res, next) => {
 }
 
 exports.update = async (req, res, next) => {
+  const category = await Category.findById(req.params.id)
+  const file = req.file
+  const protocol = req.protocol
+  const host = req.get('host')
+  const basePath = protocol + '://' + host + '/public/uploads/'
+  let imagePath
+
+  if (file) {
+    fileName = req.file.filename
+    imagePath = basePath + fileName
+  } else {
+    imagePath = category.image
+  }
   try {
-    const category = await Category.findByIdAndUpdate(
+    const updatedCategory = await Category.findByIdAndUpdate(
       req.params.id,
       {
         categoryName: req.body.categoryName,
-        image: req.body.image,
+        image: imagePath,
       },
       { new: true },
     )
-    if (!category) {
+    if (!updatedCategory) {
       throw Error('Category not found!!')
     }
-    return res.status(httpStatus.OK).json({ category })
+    return res.status(httpStatus.OK).json({ updatedCategory })
   } catch (error) {
     next(error)
   }
--- src/routes/api/category.route.js
+++ src/routes/api/category.route.js
@@ -1,19 +1,46 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+}
 
-const {validate} = require('express-validation')
-const categoryValidation = require('../../validations/category.validation');
-const categoryController = require("../../controllers/category.controller");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype]
+    let uploadError = new Error('Invalid file type')
+    if (isValid) {
+      uploadError = null
+    }
+    cb(uploadError, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(' ').join('.')
+    const extension = FILE_TYPE_MAP[file.mimetype]
+    cb(null, fileName + '-' + Date.now() + '.' + extension)
+  },
+})
 
+const uploadOptions = multer({ storage: storage })
+const { validate } = require('express-validation')
+const categoryValidation = require('../../validations/category.validation')
+const categoryController = require('../../controllers/category.controller')
 
 // create new category
-router.post('/',validate(categoryValidation.create),categoryController.create);
+router.post(
+  '/',
+  uploadOptions.single('image'),
+  validate(categoryValidation.create),
+  categoryController.create,
+)
 //  get all category data
-router.get('/list',categoryController.list);
+router.get('/list', categoryController.list)
 // get a category data
-router.get('/:id',categoryController.view);
+router.get('/:id', categoryController.view)
 //update
-router.put('/:id',categoryController.update);
+router.put('/:id', uploadOptions.single('image'), categoryController.update)
 //category delete
-router.delete('/:id',categoryController.remove);
-module.exports = router;
+router.delete('/:id', categoryController.remove)
+module.exports = router
