927e4488d990f206dc6e790290367277782e064c
Buddhika <buddhikad1999@gmail.com>
2022-05-04 00:36:46 +0530

    .

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,15 +1,35 @@
 const Product = require('../models/product.model')
 const Order = require('../models/order.model')
 const OrderItems = require('../models/order-Items.model')
+const Category = require('../models/category.model')
 const DeliveryLocations = require('../models/deliveryArea.model')
 const Customer = require('../models/customer.model')
 const httpStatus = require('http-status')
 exports.dashboard = async (req, res, next) => {
-const filter = {}
+  const filter = {}
+  let data={}
+  const date = new Date().toDateString()
+  const tomorrow =new Date(new Date(date).getTime() + 60 * 60 * 24 * 1000);
+  console.log(date,date+1);
   try {
-  
-    const orderCount =await Order.count(filter, function (err, count) {    })
-    console.log(orderCount);
+    const todayOrderCount = await Order.find({dateOrder: { $gte: date, $lte: tomorrow },status:'processing'}).count()
+    const totalOrderCount = await Order.find(filter).count()
+    const newCustomer =await Customer.find({ dateRegistered: { $gte: date, $lte: tomorrow } }).count()
+    const totalCustomer =await Customer.find(filter).count()
+    const totalProducts = await Product.count()
+    const totalCategories =await Category.count()
+    const allTodayOrders = await Order.find({ dateOrder: { $gte: date, $lte: tomorrow } })
+    let totalIncome =0
+    console.log(allTodayOrders[1].totalPrice);
+    for (let i = 0; i < allTodayOrders.length; i++){
+      totalIncome+=allTodayOrders[i].totalPrice
+    }
+    data ={totalOrderCount:totalOrderCount, totalCategories:totalCategories,newCustomer:newCustomer,todayOrderCount: todayOrderCount, totalIncome: totalIncome, totalProducts: totalProducts, totalCustomer: totalCustomer }
+    if (data) 
+      return res.status(httpStatus.OK)
+        .json({ data})
+    return res.status(httpStatus.NOT_FOUND).send('No data found')
+    
   } catch (error) {
     next(error)
   }
--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -24,7 +24,7 @@ const customerSchema = new Schema(
       required: true,
     },
     phoneNumber: {
-      type: Number,
+      type: String,
       required: true,
     },
     status: {
