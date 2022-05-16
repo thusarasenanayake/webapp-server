009378ad023123e5767704c2b540369740f798c3
Buddhika <buddhikad1999@gmail.com>
2022-05-17 01:47:00 +0530

    .

--- src/middlewares/permissionLevel.js
+++ src/middlewares/permissionLevel.js
@@ -1,33 +1,31 @@
-const httpStatus = require("http-status");
+const httpStatus = require('http-status')
 
-const Staff = require("../models/staff.model");
+const Staff = require('../models/staff.model')
 
 async function permission(USER, res, ROLE) {
-    console.log(USER,ROLE,USER.isAdmin);
-	if (ROLE === USER.isAdmin) {
-		const user = await Staff.findById(USER.userID);
-		if (!user) {
-			return res
-				.status(httpStatus.UNAUTHORIZED)
-				.json({ message: "Unauthorized Access" });
-		}
-	} 
-    else if (ROLE === USER.isAdmin) {
-		const user = await Staff.findById(USER.userId);
-		if (!user) {
-			return res
-				.status(httpStatus.UNAUTHORIZED)
-				.json({ message: "Unauthorized Access" });
-		}
-	} 
-    else {
-		const user = await Staff.findById(USER.userId);
-		if (!user) {
-			return res
-				.status(httpStatus.UNAUTHORIZED)
-				.json({ message: "Unauthorized Access" });
-		}
-	}
+  console.log(USER, ROLE, USER.isAdmin)
+  if (ROLE === USER.isAdmin) {
+    const user = await Staff.findById(USER.userID)
+    if (!user) {
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json({ message: 'Unauthorized Access' })
+    }
+  } else if (ROLE === USER.isAdmin) {
+    const user = await Staff.findById(USER.userId)
+    if (!user) {
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json({ message: 'Unauthorized Access' })
+    }
+  } else {
+    const user = await Staff.findById(USER.userId)
+    if (!user) {
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json({ message: 'Unauthorized Access' })
+    }
+  }
 }
 
-module.exports = permission;
+module.exports = permission
